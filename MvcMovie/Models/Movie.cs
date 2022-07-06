d74018c24ac4f74bb49dc9b4ccf3dd8459eed872
bfrodsham <bfrodsham@gmail.com>
2022-07-06 15:33:36 -0600

    Stopping for today

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 
 // This file defines the Movie model, which is what goes into the database.
 
@@ -10,11 +13,17 @@ namespace MvcMovie.Models
         // The ? means that this variable is nullable.
         public string? Title { get; set; }
 
-        // This attribute below makes it so that the user isn't required to enter time information in the date field, 
+
+        // These are called data annotations
+        // This annotation below makes it so that the user isn't required to enter time information in the date field, 
         // and also the time is not displayed.
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        // This is required so Entity Framework Core can correctly map the price to currency in the database
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     
     }
